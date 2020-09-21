c6c92fd3942f931c1706758ff0bd9e61bf9d520b
Mohit <mohit.padhye2011@gmail.com>
2020-09-21 16:07:03 +0545

    added adminlite dashboard |demo page

--- cms/urls.py
+++ cms/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path
 
+from cms import settings
+from student_management_app import views
+
 urlpatterns = [
+    path('demo',views.showDemoPage),
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+                                                                                                                           
